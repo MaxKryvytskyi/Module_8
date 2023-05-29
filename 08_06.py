205cdc90fde855c5ba3f4fc73e356f4510d3b938
Max <maxkryvytskyiwork@gmail.com>
2023-05-30 01:49:46 +0300

    push

--- 08_06.py
+++ 08_06.py
@@ -3,11 +3,12 @@ from decimal import Decimal, getcontext
 number_list = [4.5788689699797, 34.7576578697964, 86.8877666656633, 12]
 signs_count = 6
 def decimal_average(number_list, signs_count):
-
-
-
-
-decimal_average(number_list, signs_count)
+    getcontext().prec = signs_count
+    sums = Decimal(0)
+    for i in number_list:
+        sums += Decimal(i)
+    return sums/len(number_list)
+print(decimal_average(number_list, signs_count))
     
     
     
